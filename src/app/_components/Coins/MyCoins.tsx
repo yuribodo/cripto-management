74ef01432b71557966f7b9e08697e68d7066695a
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-08-26 07:22:58 -0300

    feat: add adjustment to mycoins

--- src/app/_components/Coins/MyCoins.tsx
+++ src/app/_components/Coins/MyCoins.tsx
@@ -2,9 +2,14 @@ import CoinsCard from "./CoinsCard";
 
 export default function MyCoins() {
     return (
-        <div className="flex flex-col bg-slate-500">
-            <h1 className=" font-bold">My Coins:</h1>
-            <CoinsCard/>
+        <div className="flex flex-col bg-slate-200 min-h-screen">
+            <h1 className=" font-bold p-6 text-xl">My Coins:</h1>
+            <div className="flex flex-col justify-center items-center space-y-6">
+                <CoinsCard/>
+                <CoinsCard/>
+                <CoinsCard/>
+            </div>
+            
         </div>
     );
 }
