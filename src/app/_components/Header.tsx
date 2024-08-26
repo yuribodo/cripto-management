cce54494e77e37eab2d6dd330b550e5956d5ae27
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-08-26 07:00:45 -0300

    feat: Header Component initial design

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -2,14 +2,20 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <div className="flex">
-             <Image
-                src="/profile.png"
-                width={10}
-                height={10}
-                alt="Picture of the author"
-            />
-            <p>Account</p>
+        <div className="flex justify-between items-center bg-slate-200 p-4">
+            <div className="flex ">
+                <Image
+                    src="/profile.png"
+                    width={10}
+                    height={10}
+                    alt="Picture of the author"
+                />
+                <p>Account</p>
+            </div>
+            
+
+            <h1 className="">Option 1</h1>
+            <h1>Option 2</h1>
         </div>
     );
 }
