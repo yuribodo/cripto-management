c28611d747bcbdbbc962e64e0083acdfe85d0ea1
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-08-26 22:17:42 -0300

    feat: improve my coins design

--- src/app/_components/Coins/CoinsCard.tsx
+++ src/app/_components/Coins/CoinsCard.tsx
@@ -2,19 +2,20 @@ import Image from "next/image";
 
 export default function CoinsCard() {
     return (
-        <div className=" flex flex-col bg-green-50 w-[80vw] p-6 rounded-2xl ">
-            <div className="flex  justify-between">
-                <div className="flex">
+        <div className="flex flex-col bg-gray-800 w-[80vw] p-6 rounded-2xl text-white">
+            <div className="flex justify-between">
+                <div className="flex items-center">
                     <Image
-                        src="/profile.png"
-                        width={10}
-                        height={10}
-                        alt="Picture of the author"
+                        src="/bitcoin.png"
+                        width={40}
+                        height={40}
+                        alt="Coin image"
+                        className="mr-4"
                     />
-                    <h1 className=" font-semibold text-xl">Bitcoin</h1>
+                    <h1 className="font-semibold text-xl">Bitcoin</h1>
                 </div>
                 <div>
-                    <h1 className=" p-2 font-bold">R$: 300000</h1>
+                    <h1 className="p-2 font-bold">R$: 300000</h1>
                 </div>
             </div>
         </div>
--- src/app/_components/Coins/MyCoins.tsx
+++ src/app/_components/Coins/MyCoins.tsx
@@ -2,14 +2,13 @@ import CoinsCard from "./CoinsCard";
 
 export default function MyCoins() {
     return (
-        <div className="flex flex-col bg-slate-200 min-h-screen">
-            <h1 className=" font-bold p-6 text-xl">My Coins:</h1>
+        <div className="flex flex-col bg-gray-900 min-h-screen">
+            <h1 className="font-bold p-6 text-xl text-white">My Coins:</h1>
             <div className="flex flex-col justify-center items-center space-y-6">
-                <CoinsCard/>
-                <CoinsCard/>
-                <CoinsCard/>
+                <CoinsCard />
+                <CoinsCard />
+                <CoinsCard />
             </div>
-            
         </div>
     );
 }
