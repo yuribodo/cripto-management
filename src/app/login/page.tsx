63cc1948e4fe9f1875ed060664912931d9a4acba
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-09-08 17:41:54 -0400

    feat: add page login and planos initla design

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+    return (
+      <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">Faça Login</h1>
+        <p className="mt-4 text-lg max-w-2xl text-center">
+          Entre na sua conta para começar a gerenciar suas criptomoedas.
+        </p>
+      </div>
+    );
+  }
+  
